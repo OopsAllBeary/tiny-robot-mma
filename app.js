9fd9b1b75190049266057c7d03679c6c005e5f4d
Bear Collins <bcollins@maxmedia.com>
2020-04-22 17:12:45 -0400

    port update

--- app.js
+++ app.js
@@ -93,6 +93,6 @@ io.on('connection', function (socket) {
   });
 });
 
-server.listen(8081, function () {
+server.listen(80, function () {
   console.log(`Listening on ${server.address().port}`);
 });
